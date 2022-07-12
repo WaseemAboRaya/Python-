df6d4552adff35cb24ad107f78464121ac171505
WaseemAboRaya <aboraya.w@gmail.com>
2022-07-13 00:51:29 +0300

    the password_validator is completed

--- password_validator.py
+++ password_validator.py
@@ -1,10 +1,19 @@
 import colorama
 
-def printresult(s:str):
-    r=goodpass(s)
-    if r:print(colorama.Fore.GREEN+"the pass is good")
-    else :print(colorama.Fore.RED+"the pass is not good")
-def goodpass(s:str) -> bool:
+
+def printresult(s: str) -> bool:
+    r = goodpass(s)
+    if r:
+        print(colorama.Fore.GREEN + "the pass is good")
+        return 0
+    else:
+        return 1
+
+
+def goodpass(s: str) -> bool:
+    if s == "":
+        print(colorama.Fore.RED + "the pass is empty")
+        return False
     Cap, small, numbers, c = 0, 0, 0, 0
     for ch in s:
         c += 1
@@ -12,12 +21,14 @@ def goodpass(s:str) -> bool:
         if 'z' >= ch >= 'a': small += 1
         if '9' >= ch >= '0': numbers += 1
         if Cap > 0 and small > 0 and numbers > 0 and c >= 10: return True
+    if c < 10: print(colorama.Fore.RED + "Length – minimum of 10 characters")
+    if Cap == 0: print(colorama.Fore.RED + "there are no capital letters")
+    if small == 0: print(colorama.Fore.RED + "there are no small letters")
+    if small == 0: print(colorama.Fore.RED + "there are no number letters")
     return Cap > 0 and small > 0 and numbers > 0 and c >= 10
 
 
-s1 = "asfrdFasc152"
-s2 = "a12548521d545"
-s3 = "waAs98"
-printresult(s1)
-printresult(s2)
-printresult(s3)
+with open('expass.txt', 'r') as f:
+    for x in f.readlines():
+        r1 = printresult(x)
+        print(r1)
