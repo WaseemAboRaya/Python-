eb9344ac214324b3755f363c605f60733d3bd752
WaseemAboRaya <aboraya.w@gmail.com>
2022-07-12 23:38:25 +0300

    the password_validator is copleted

--- password_validator.py
+++ password_validator.py
@@ -1,1 +1,23 @@
-print("waseem")
+import colorama
+
+def printresult(s:str):
+    r=goodpass(s)
+    if r:print(colorama.Fore.GREEN+"the pass is good")
+    else :print(colorama.Fore.RED+"the pass is not good")
+def goodpass(s:str) -> bool:
+    Cap, small, numbers, c = 0, 0, 0, 0
+    for ch in s:
+        c += 1
+        if 'Z' >= ch >= 'A': Cap += 1
+        if 'z' >= ch >= 'a': small += 1
+        if '9' >= ch >= '0': numbers += 1
+        if Cap > 0 and small > 0 and numbers > 0 and c >= 10: return True
+    return Cap > 0 and small > 0 and numbers > 0 and c >= 10
+
+
+s1 = "asfrdFasc152"
+s2 = "a12548521d545"
+s3 = "waAs98"
+printresult(s1)
+printresult(s2)
+printresult(s3)
